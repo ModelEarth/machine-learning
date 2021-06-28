b75726653834428c4a525e857147c05b681d70e2
John Taylor <johnt154@gmail.com>
2021-06-28 11:18:14 +0300

    Minor edits to comments

--- extract_BLS_QCEW.py
+++ extract_BLS_QCEW.py
@@ -11,13 +11,13 @@ df = pd.read_csv("FIPS_state.csv", dtype = str)
 fips_list = df['FIPS'].tolist()
 
 # Year that data will be requested.
-# This code uses annual averages, but quarterly data can also be extracted
+# This code uses annual averages, but quarterly data can also be extracted.
 year = "2020"
 
 for fips in fips_list:
-    # This is the direct URL to access the BLS employment data
+    # This is the direct URL to access the BLS employment data.
     url = 'https://data.bls.gov/cew/data/api/'+year+'/a/area/'+fips+'000.csv'
-    # File from the URL will be saved in the preexisting local folder
+    # File from the URL will be saved in the preexisting local folder.
     urllib.request.urlretrieve(url, 'C:/Users/Beetle/Dropbox (Personal)/Internship/Data_'+year+'/'+fips+'000.csv')
     print("Completed "+fips)
 
