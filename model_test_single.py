3f92c37d066393aa894e36fea6dcfbac5fb26fa5
John Taylor <johnt154@gmail.com>
2021-06-28 11:38:06 +0300

    Added version 1.2
    This version handles multiple years of data and handles the IndexError thrown when a NAICS code is missing from a FIPS's CSV file.

--- model_test_single.py
+++ model_test_single.py
@@ -1,10 +1,9 @@
-# model_test_single v1.1
+# model_test_single v1.2
 # Trains random MLP models for a single NAICS code and outputs statistics.
 # John Andrew Taylor, June 2021
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.neural_network import MLPRegressor
 from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
@@ -12,25 +11,31 @@ import statistics
 
 # NAICS code for which model is being made
 NAICS = "562211"
-# State
-state = "13"
+# Years to draw data from (local storage)
+years = ["2020", "2019"]
 
 # A list of the 2-digit state FIPS codes is given to import extracted BLS data.
 # (see extract_BLS_QCEW_v2.py)
 df = pd.read_csv("FIPS_state.csv", dtype = str)
 fips_list = df['FIPS'].tolist()
-fips_list.remove(state)
 
 # Build X and y sets
 X = []
 y = []
 
-for fips in fips_list:
-	df = pd.read_csv("Data/"+fips+"000.csv", usecols = ["industry_code", "annual_avg_estabs", "annual_avg_emplvl", "total_annual_wages"])
-	df = df.loc[df["industry_code"] == NAICS]
-	if df.iloc[0]["annual_avg_emplvl"] != 0 or df.iloc[0]["annual_avg_estabs"] == 0:
-		X.append([df.iloc[0]["annual_avg_estabs"]])
-		y.append(df.iloc[0]["annual_avg_emplvl"])
+# Iterates through each Data_XXXX folder
+for year in years:
+	# Iterates through each FIPS XXXXX.csv file
+	for fips in fips_list:
+		df = pd.read_csv("Data_"+year+"/"+fips+"000.csv", usecols = ["industry_code", "annual_avg_estabs", "annual_avg_emplvl", "total_annual_wages"])
+		# Try-except block needed in case NAICS code is not present in a CSV file
+		try:
+			df = df.loc[df["industry_code"] == NAICS]
+			if df.iloc[0]["annual_avg_emplvl"] != 0 or df.iloc[0]["annual_avg_estabs"] == 0:
+				X.append([df.iloc[0]["annual_avg_estabs"]])
+				y.append(df.iloc[0]["annual_avg_emplvl"])
+		except IndexError:
+			print("Missing NAICS code "+NAICS+" in "+year+" data of "+fips+"000.csv")
 
 print("Dataset size: " + str(len(X)))
 
